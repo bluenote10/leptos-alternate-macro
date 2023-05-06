468518302a3aab7234e954f64ca23bd5dd1b60d4
Fabian Keller <github.100.fkeller@spamgourmet.com>
2023-05-06 12:07:15 +0200

    rename library workspace folder

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -9,15 +9,6 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn comp(input: TokenStream) -> TokenStream {
-    let input = proc_macro2::TokenStream::from(input);
-
-    let output = macro_impl::comp(input);
-
-    proc_macro::TokenStream::from(output)
-}
-
-#[proc_macro]
 pub fn c(input: TokenStream) -> TokenStream {
     let input = proc_macro2::TokenStream::from(input);
 
--- lib/src/macro_impl.rs
+++ lib/src/macro_impl.rs
@@ -4,61 +4,13 @@ use quote::quote;
 use syn::{
     parse::{Parse, ParseStream},
     punctuated::Punctuated,
-    Error, Expr, FieldValue, Ident, Member, Result, Token,
+    Error, Expr, FieldValue, Ident, Member, Result,
 };
-
-pub fn comp(input: TokenStream) -> TokenStream {
-    println!("raw input:\n{:#?}", input);
-
-    // let input: TokenTree = Group::new(Delimiter::Parenthesis, input).into();
-    // let input: TokenStream = input.into();
-    // println!("wrapped input:\n{:#?}", input);
-
-    match syn::parse2::<CompBody>(input) {
-        Ok(comp_body) => {
-            println!("{:#?}", comp_body);
-
-            quote! {
-                log!("Hello world from macro, using parent scope symbol: {}", style);
-            }
-            .into()
-        }
-        Err(err) => {
-            //quote_spanned!(err.span() => compile_error!("Could not parse token stream as expression")).into()
-            err.into_compile_error()
-        }
-    }
-
-    /*
-    let input_span = input.span();
-    let Ok(s) = syn::parse2::<Expr>(input) else {
-        return quote_spanned!(input_span => compile_error!("Could not parse token stream as expression")).into()
-    };
-    */
-}
-
-#[derive(Debug)]
-struct CompBody {
-    expressions: Vec<Expr>,
-}
-
-impl Parse for CompBody {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let expressions: Vec<_> = input
-            .parse_terminated(Expr::parse, Token![,])?
-            .into_iter()
-            .collect();
-        Ok(CompBody { expressions })
-    }
-}
 
 pub fn macro_c(input: TokenStream) -> TokenStream {
     match syn::parse2::<CompExpr>(input) {
-        Ok(comp_expr) => transform_comp(comp_expr),
-        Err(err) => {
-            //quote_spanned!(err.span() => compile_error!("Could not parse token stream as expression")).into()
-            err.into_compile_error()
-        }
+        Ok(comp_expr) => transform(comp_expr),
+        Err(err) => err.into_compile_error(),
     }
 }
 
@@ -106,7 +58,7 @@ impl Parse for CompExpr {
     }
 }
 
-fn transform_comp(comp_expr: CompExpr) -> TokenStream {
+fn transform(comp_expr: CompExpr) -> TokenStream {
     let ident = comp_expr.ident;
     let is_element = ident.to_string().chars().next().unwrap().is_lowercase();
 
@@ -173,33 +125,6 @@ mod tests {
     use super::*;
     use crate::test_utils::compare;
     use pretty_assertions::assert_eq;
-
-    fn parse_comp_body(input: TokenStream) -> Result<CompBody> {
-        syn::parse2::<CompBody>(input)
-    }
-
-    #[test]
-    fn test_parse_comp_body() {
-        let stream = quote!();
-        let result = parse_comp_body(stream).unwrap();
-        assert_eq!(result.expressions.len(), 0);
-
-        let stream = quote!(foo);
-        let result = parse_comp_body(stream).unwrap();
-        assert_eq!(result.expressions.len(), 1);
-
-        let stream = quote!(foo,);
-        let result = parse_comp_body(stream).unwrap();
-        assert_eq!(result.expressions.len(), 1);
-
-        let stream = quote!(foo, bar);
-        let result = parse_comp_body(stream).unwrap();
-        assert_eq!(result.expressions.len(), 2);
-
-        let stream = quote!(foo, bar,);
-        let result = parse_comp_body(stream).unwrap();
-        assert_eq!(result.expressions.len(), 2);
-    }
 
     fn parse_comp_expr(input: TokenStream) -> Result<CompExpr> {
         syn::parse2::<CompExpr>(input)
